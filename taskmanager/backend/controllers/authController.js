ed0910eaf759502dc444c2e5d542df97c516dbba
N1KHIL-CHOUDHARY <nikhil2k7h@gmail.com>
2026-02-26 10:27:57 +0530

    new

--- taskmanager/backend/controllers/authController.js
+++ taskmanager/backend/controllers/authController.js
@@ -11,8 +11,8 @@ const sendTokenResponse = (user, res) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: 'strict',
+    secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 
